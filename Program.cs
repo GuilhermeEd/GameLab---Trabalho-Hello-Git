2b5f7f27fc4f187c944939ce970c92462f50ff28
GuilhermeEd <guilherme.eduardo92@hotmail.com>
2019-03-24 19:03:58 -0300

    Início

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Aplicativo_do_Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Git:)");
+        }
+    }
+}
